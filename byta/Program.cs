ea83f00d912d1b7dba73eccc750fd2f1e8553b25
newe08001 <newe08001@E-PW05SJ7Q>
2025-10-08 13:14:25 +0200

    Add project files.

---
+++ byta/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace byta;
+
+class program
+{ 
+    static void Main(string[]args)
+
+    {
+        Console.WriteLine("Hur gammal e du?");
+        Console.Write("Skriv din ålder här: ");
+        int alder = int .Parse(Console.ReadLine());
+        if (  alder >= 16 && alder <=19 )
+
+        { Console.WriteLine("Du får delta i tävlingen!"); }
+        else 
+        { Console.WriteLine("Tyvärr, du får inte delta i tävlingen!"); }
+
+
+    }
+
+
+
+
+
+
+    
+}   
+
+
